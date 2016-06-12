eb1d75dd84ffdcb2f5cbdd2681825d8c6030083c
Robert Black <rbla4655@gmail.com>
2016-06-12 19:06:17 -0400

    heroku deploy

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -5,23 +5,22 @@ var helpers = {
 		return axios.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query.term + "&begin_date=" + query.start + "0101&end_date=" + query.end + "1231&api-key=e05abfb61c5346fea5bf75beb92d714e")
 			.then(function(data){
 				return callback(data);
-				console.log(data)
 			})
 	},
 	getSavedArticles: function(callback){
-		return axios.get("http://localhost:8080/api/all")
+		return axios.get("http://rb-nyt-search.herokuapp.com/api/all")
 			.then(function(data){
 				return callback(data);
 			})
 	},
 	addArticle: function(article, callback){
-		return axios.post("http://localhost:8080/api/add", article)
+		return axios.post("http://rb-nyt-search.herokuapp.com/api/add", article)
 			.then(function(response){
 				return callback()
 			})
 	},
 	deleteArticle: function(id, callback){
-		return axios.post("http://localhost:8080/api/delete", id)
+		return axios.post("http://rb-nyt-search.herokuapp.com/api/delete", id)
 			.then(function(response){
 				return callback()
 			})
